588022a596e73bb6b5abadf55619e231409c88cb
desertgm <bulbogc@gmail.com>
2024-10-13 21:46:40 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useLogger(['error', 'warn', 'log', 'debug']);
   app.useGlobalPipes(
     new ValidationPipe({
